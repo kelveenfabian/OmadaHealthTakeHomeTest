0695df22964969e7fbd7bcf9d7534c9140c2c4bf
Kelveen Fabian <43725042+kelveenfabian@users.noreply.github.com>
2025-11-21 12:16:38 -0500

    Create NetworkClient class that will fetch all of our api services

---
+++ app/src/main/java/com/example/omadahealthtakehometest/data/network/NetworkClient.kt
@@ -0,0 +1,28 @@
+package com.example.omadahealthtakehometest.data.network
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object NetworkClient {
+    private const val BASE_URL = "https://www.flickr.com"
+    private val loggingInterceptor = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+    private val okHttpClient = OkHttpClient.Builder()
+        .addInterceptor(loggingInterceptor)
+        .build()
+
+    private val retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
+            .build()
+    }
+
+    fun <T> createService(serviceClass: Class<T>): T {
+        return retrofit.create(serviceClass)
+    }
+}
