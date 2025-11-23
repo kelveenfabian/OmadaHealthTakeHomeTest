a1c7c60a88d38636275ddfa6a55a793733d8568c
Kelveen Fabian <43725042+kelveenfabian@users.noreply.github.com>
2025-11-22 20:42:17 -0500

    Add networkResultHandler() to NetworkResult.kt

--- app/src/main/java/com/example/omadahealthtakehometest/data/network/NetworkResult.kt
+++ app/src/main/java/com/example/omadahealthtakehometest/data/network/NetworkResult.kt
@@ -1,7 +1,27 @@
 package com.example.omadahealthtakehometest.data.network
 
+import retrofit2.HttpException
+import retrofit2.Response
+import java.io.IOException
+
 sealed interface NetworkResult<out T> {
     data class Error(val exception: Throwable, val message: String): NetworkResult<Nothing>
     data class Success<out T>(val data: T): NetworkResult<T>
     object Empty : NetworkResult<Nothing>
+}
+
+suspend fun <T> networkResultHandler(apiCall: suspend () -> Response<T>): NetworkResult<T> {
+    return try {
+        val response = apiCall()
+        if (response.isSuccessful && response.body() != null) {
+            NetworkResult.Success(response.body()!!)
+        } else {
+            val errorBody = response.errorBody().toString()
+            NetworkResult.Error(HttpException(response), errorBody)
+        }
+    } catch (e: IOException) {
+        NetworkResult.Error(e, "Network error: Check your internet connection")
+    } catch (e: Exception) {
+        NetworkResult.Error(e, "An unexpected error occurred")
+    }
 }
