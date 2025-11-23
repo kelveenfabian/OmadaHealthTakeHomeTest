41590a6f730675a0cd32b6c0ecc43a84ac174201
Kelveen Fabian <43725042+kelveenfabian@users.noreply.github.com>
2025-11-22 23:27:38 -0500

    Create FlickrScreen

--- app/src/main/java/com/example/omadahealthtakehometest/MainActivity.kt
+++ app/src/main/java/com/example/omadahealthtakehometest/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.viewModels
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -12,48 +11,39 @@ import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SearchBar
-import androidx.compose.material3.SearchBarDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.ViewModelProvider
+import androidx.hilt.navigation.compose.hiltViewModel
 import coil3.compose.AsyncImage
 import coil3.request.ImageRequest
 import coil3.request.crossfade
+import com.example.omadahealthtakehometest.domain.model.PhotoItem
+import com.example.omadahealthtakehometest.ui.FlickrUiEvent
+import com.example.omadahealthtakehometest.ui.FlickrUiState
+import com.example.omadahealthtakehometest.ui.FlickrViewModel
 import com.example.omadahealthtakehometest.ui.theme.OmadaHealthTakeHomeTestTheme
-import com.example.omadahealthtakehometest.ui.viewmodel.FlickrUiState
-import com.example.omadahealthtakehometest.ui.viewmodel.FlickrViewModel
-import com.example.omadahealthtakehometest.ui.viewmodel.FlickrViewModelFactory
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    private lateinit var viewModel: FlickrViewModel
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        viewModel = FlickrViewModelFactory().create(FlickrViewModel::class.java)
         enableEdgeToEdge()
         setContent {
             OmadaHealthTakeHomeTestTheme {
@@ -63,7 +53,6 @@ class MainActivity : ComponentActivity() {
                         .background(Color.Gray)
                 ) { innerPadding ->
                     FlickrScreen(
-                        viewModel = viewModel,
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -72,100 +61,95 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun FlickrScreen(viewModel: FlickrViewModel, modifier: Modifier) {
-    val uiState = viewModel.uiState.collectAsState()
-    when (val state = uiState.value) {
-        is FlickrUiState.Error -> {
-            Text(text = state.message)
-        }
+fun FlickrScreen(viewModel: FlickrViewModel = hiltViewModel(), modifier: Modifier) {
+    val uiState by viewModel.uiState.collectAsState()
+    val searchQuery by viewModel.searchQuery.collectAsState()
 
-        FlickrUiState.Loading -> {
-            Box(
-                modifier = modifier.fillMaxSize(),
-                contentAlignment = Alignment.Center
-            ) {
-                CircularProgressIndicator()
+    when (val state = uiState) {
+        is FlickrUiState.Error -> ErrorContent(modifier, state.message)
+
+
+        FlickrUiState.Loading -> LoadingContent(modifier)
+
+
+        is FlickrUiState.Success -> SuccessContent(
+            data = state.data,
+            modifier = modifier,
+            viewModel = viewModel,
+            searchQuery = searchQuery,
+        )
+    }
+}
+
+@Composable
+private fun ErrorContent(modifier: Modifier, message: String) {
+    Box(
+        modifier = modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "Error: $message")
+    }
+}
+
+@Composable
+private fun LoadingContent(modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        CircularProgressIndicator()
+    }
+}
+
+@Composable
+private fun SuccessContent(
+    data: List<PhotoItem>,
+    modifier: Modifier = Modifier,
+    viewModel: FlickrViewModel,
+    searchQuery: String
+) {
+    Column(
+        modifier = modifier.fillMaxSize()
+    ) {
+        TextField(
+            value = searchQuery,
+            onValueChange = {
+                viewModel.onFlickrUserEvent(FlickrUiEvent.UpdateSearchQuery(it))
+            },
+            label = { Text(text = "Search") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            singleLine = true,
+        )
+
+        LazyVerticalGrid(
+            columns = GridCells.Adaptive(minSize = 128.dp)
+        ) {
+            items(data) { photoItem ->
+                FlickrPhotoItem(photoItem = photoItem)
             }
         }
+    }
+}
 
-        is FlickrUiState.Success -> {
-
-            var expanded by rememberSaveable { mutableStateOf(false) }
-
-            Column(
-                modifier = Modifier.fillMaxSize()
-            ) {
-                SearchBar(
-                    modifier = Modifier.fillMaxWidth().padding(bottom = 16.dp),
-                    inputField = {
-                        SearchBarDefaults.InputField(
-                            query = "Search",
-                            onQueryChange = { "Yo" },
-                            onSearch = {
-                                { "yo" } //Add MainEventUi to VM
-                                expanded = false
-                            },
-                            expanded = expanded,
-                            onExpandedChange = { expanded = it },
-                            placeholder = { Text("Search") }
-                        )
-                    },
-                    expanded = expanded,
-                    onExpandedChange = { expanded = it }
-                ) {
-                    LazyVerticalGrid(
-                        modifier = modifier.fillMaxSize(),
-                        columns = GridCells.Adaptive(minSize = 128.dp)
-                    ) {
-                        items(state.data) { photoItem ->
-
-                            Box(
-                                modifier = Modifier
-                                    .aspectRatio(1f)
-                                    .clip(RectangleShape)
-                                    .padding(4.dp),
-                                contentAlignment = Alignment.Center
-                            ) {
-                                AsyncImage(
-                                    model = ImageRequest.Builder(LocalContext.current)
-                                        .data(photoItem.url)
-                                        .crossfade(true)
-                                        .build(),
-                                    contentDescription = "Image from Flickr",
-                                    contentScale = ContentScale.Crop,
-                                )
-                            }
-                        }
-                    }
-                }
-
-                LazyVerticalGrid(
-                    columns = GridCells.Adaptive(minSize = 128.dp)
-                ) {
-                    items(state.data) { photoItem ->
-
-                        Box(
-                            modifier = Modifier
-                                .aspectRatio(1f)
-                                .clip(RectangleShape)
-                                .padding(4.dp),
-                            contentAlignment = Alignment.Center
-                        ) {
-                            AsyncImage(
-                                model = ImageRequest.Builder(LocalContext.current)
-                                    .data(photoItem.url)
-                                    .crossfade(true)
-                                    .build(),
-                                contentDescription = "Image from Flickr",
-                                contentScale = ContentScale.Crop,
-                            )
-                        }
-                    }
-                }
-            }
-
-        }
+@Composable
+private fun FlickrPhotoItem(photoItem: PhotoItem) {
+    Card(
+        modifier = Modifier
+            .aspectRatio(1f)
+            .padding(4.dp),
+        elevation = CardDefaults.cardElevation(defaultElevation = 2.dp)
+    ) {
+        AsyncImage(
+            model = ImageRequest.Builder(LocalContext.current)
+                .data(photoItem.url)
+                .crossfade(true)
+                .build(),
+            contentDescription = "Image from Flickr",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize()
+        )
     }
 }
