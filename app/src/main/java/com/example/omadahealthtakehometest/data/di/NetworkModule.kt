6e4b0dea95ff92c9266e4100031a876935d82ba4
Kelveen Fabian <43725042+kelveenfabian@users.noreply.github.com>
2025-11-22 20:40:07 -0500

    Create NetworkModule.kt

---
+++ app/src/main/java/com/example/omadahealthtakehometest/data/di/NetworkModule.kt
@@ -0,0 +1,47 @@
+package com.example.omadahealthtakehometest.data.di
+
+import com.example.omadahealthtakehometest.data.network.FlickrApiService
+import com.example.omadahealthtakehometest.data.network.interceptor.AuthInterceptor
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideAuthInterceptor(): AuthInterceptor {
+        return AuthInterceptor()
+    }
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(authInterceptor: AuthInterceptor): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(authInterceptor)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .client(okHttpClient)
+            .baseUrl("https://www.flickr.com")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideFlickrApiService(retrofit: Retrofit): FlickrApiService {
+        return retrofit.create(FlickrApiService::class.java)
+    }
+}
